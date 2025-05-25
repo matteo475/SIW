4102790f3eda23867effd930d2e5c355a07efebe
lucam <lucam@Lenovoluca.homenet.telecomitalia.it>
2025-05-25 15:31:02 +0200

    aggiunto ricerca filtrata

--- Ecommerce/src/main/java/it/uniroma3/Ecommerce/repository/ProductRepository.java
+++ Ecommerce/src/main/java/it/uniroma3/Ecommerce/repository/ProductRepository.java
@@ -4,7 +4,8 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import it.uniroma3.Ecommerce.model.Product;
 
@@ -15,5 +16,10 @@ public interface ProductRepository extends JpaRepository<Product, Integer>{
 	public Long countById(Long id);
 	public Optional<Product> findById(Long id);
 	//qui implementeremo le query per cercare i prodotti con una keyword
+
+@Query("SELECT p FROM Product p WHERE " +"(:name IS NULL OR :name = '' OR p.name = :name) AND " +"(:brand IS NULL OR :brand = '' OR p.brand = :brand) AND " + "(:category IS NULL OR :category = '' OR p.category = :category)")
+
+			List<Product> searchProducts(@Param("name") String name, @Param("brand") String brand,@Param("category") String category);
+
 	
 }
