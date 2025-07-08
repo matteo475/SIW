dfe06f21e2b71485a5d23c2642801d294c15cc68
matteo475 <mserra266@gmail.com>
2025-07-08 23:39:51 +0200

    piccole migliorie

--- Ecommerce/src/main/java/it/uniroma3/Ecommerce/model/Carrello.java
+++ Ecommerce/src/main/java/it/uniroma3/Ecommerce/model/Carrello.java
@@ -18,80 +18,76 @@ import jakarta.persistence.JoinColumn;
 @Entity
 public class Carrello {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    @OneToMany(mappedBy = "carrello",
-               cascade = CascadeType.REMOVE,
-               fetch = FetchType.EAGER)
-    private List<CarrelloItem> prodotti = new ArrayList<>();
+	@OneToMany(mappedBy = "carrello", cascade = CascadeType.REMOVE, fetch = FetchType.EAGER)
+	private List<CarrelloItem> prodotti = new ArrayList<>();
 
-    private double spesaTotale = 0;
+	private double spesaTotale = 0;
 
-    // Nuovo mapping bidirezionale verso User
-    @OneToOne
-    @JoinColumn(name = "user_id", nullable = false, unique = true)
-    private User user;
+	// Nuovo mapping bidirezionale verso User
+	@OneToOne
+	@JoinColumn(name = "user_id", nullable = false, unique = true)
+	private User user;
 
-    public Carrello() { }
+	public Carrello() {
+	}
 
-    public Long getId() {
-        return id;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public List<CarrelloItem> getProdotti() {
-        return prodotti;
-    }
+	public List<CarrelloItem> getProdotti() {
+		return prodotti;
+	}
 
-    public double getSpesaTotale() {
-        return spesaTotale;
-    }
+	public double getSpesaTotale() {
+		return spesaTotale;
+	}
 
-    public User getUser() {
-        return user;
-    }
+	public User getUser() {
+		return user;
+	}
 
-    public void setUser(User user) {
-        this.user = user;
-    }
+	public void setUser(User user) {
+		this.user = user;
+	}
 
-    public void addProdottoCarrello(CarrelloItem prodotto) {
-        prodotto.setCarrello(this);
-        this.prodotti.add(prodotto);
-        this.calcolaSpesaTotale();
-    }
+	public void addProdottoCarrello(CarrelloItem prodotto) {
+		prodotto.setCarrello(this);
+		this.prodotti.add(prodotto);
+		this.calcolaSpesaTotale();
+	}
 
-    public void rimuoviProdottoDalCarrello(CarrelloItem item) {
-        this.prodotti.remove(item);
-        item.setCarrello(null);
-        this.calcolaSpesaTotale();
-    }
+	public void rimuoviProdottoDalCarrello(CarrelloItem item) {
+		this.prodotti.remove(item);
+		item.setCarrello(null);
+		this.calcolaSpesaTotale();
+	}
 
-    public double calcolaSpesaTotale() {
-        this.spesaTotale = prodotti.stream()
-            .mapToDouble(ci -> ci.getPrezzoPerUnita() * ci.getQuantita())
-            .sum();
-        return spesaTotale;
-    }
+	public double calcolaSpesaTotale() {
+		this.spesaTotale = prodotti.stream().mapToDouble(ci -> ci.getPrezzoPerUnita() * ci.getQuantita()).sum();
+		return spesaTotale;
+	}
 
-    public CarrelloItem getProdottoDalCarrello(Long id) {
-        return prodotti.stream()
-            .filter(ci -> ci.getProduct().getId().equals(id))
-            .findFirst()
-            .orElse(null);
-    }
+	public CarrelloItem getProdottoDalCarrello(Long id) {
+		return prodotti.stream().filter(ci -> ci.getProduct().getId().equals(id)).findFirst().orElse(null);
+	}
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Carrello)) return false;
-        Carrello other = (Carrello) o;
-        return Objects.equals(id, other.id);
-    }
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (!(o instanceof Carrello))
+			return false;
+		Carrello other = (Carrello) o;
+		return Objects.equals(id, other.id);
+	}
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
 }
